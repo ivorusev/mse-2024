3d80739543e8c453a6467aa92d48be81c6d7652b
ivo.rusev <ivo.rusev@ontotext.com>
2023-11-26 15:17:01 +0200

    intial commit

---
+++ src/main/java/com/mse/intoduction/drawer/shapes/Shape.java
@@ -0,0 +1,14 @@
+package com.mse.intoduction.drawer.shapes;
+
+import lombok.*;
+
+import java.awt.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Shape {
+
+    protected Point point;
+}
---
+++ src/test/java/com/mse/intoduction/advancedrobot/AdvancedRobotTest.java
@@ -0,0 +1,70 @@
+package com.mse.intoduction.advancedrobot;
+
+import kareltherobot.Directions;
+import kareltherobot.Robot;
+import kareltherobot.World;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AdvancedRobotTest {
+
+    @Before
+    public void init() {
+        World.setSize(3,3);
+        World.setDelay(0);
+    }
+
+    @Test
+    public void testTurnRight() {
+        AdvancedKarel robot = new AdvancedKarel(2, 2, Directions.East);
+        robot.turnRight();
+        assertTrue(robot.facingSouth());
+    }
+
+    @Test
+    public void testMoveWithCoordinatesMoveEast() {
+        AdvancedKarel robot = new AdvancedKarel(2, 2, Directions.East);
+        robot.move();
+        assertEquals(robot.getPoint().x, 3);
+    }
+
+    @Test
+    public void testMoveWithCoordinatesMoveWest() {
+        AdvancedKarel robot = new AdvancedKarel(2, 2, Directions.West);
+        robot.move();
+        assertEquals(robot.getPoint().x, 1);
+    }
+
+    @Test
+    public void testMoveWithCoordinatesMoveSouth() {
+        AdvancedKarel robot = new AdvancedKarel(2, 2, Directions.South);
+        robot.move();
+        assertEquals(robot.getPoint().y, 1);
+    }
+
+    @Test
+    public void testMoveWithCoordinatesMoveNorth() {
+        AdvancedKarel robot = new AdvancedKarel(2, 2, Directions.North);
+        robot.move();
+        assertEquals(robot.getPoint().y, 3);
+    }
+
+    @Test
+    public void testTurnTo() {
+        AdvancedKarel robot = new AdvancedKarel(2, 2, Directions.North);
+        robot.turnTo(Directions.East);
+        assertTrue(robot.facingEast());
+    }
+
+    @Test
+    public void testMoveTo() {
+        AdvancedKarel robot = new AdvancedKarel(1, 1, Directions.East);
+        robot.moveTo(3, 3);
+        assertEquals(robot.getPoint().x, 3);
+        assertEquals(robot.getPoint().y, 3);
+        assertTrue(robot.facingEast());
+    }
+}
